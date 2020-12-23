840a5abe855c5cdb66c480442f46b27c69492790
Danecoder25 <danielbackstrom@telia.com>
2020-12-23 11:14:18 +0100

    201223

--- src/app/operator/operator.component.ts
+++ src/app/operator/operator.component.ts
@@ -10,14 +10,12 @@ export class OperatorComponent implements OnInit {
   @Input() public parentData: string;
   public transport = ['Truckförare', 'Lastkopplare'];
   public commissioning = ['Elinstallatör', 'Mekaniker', 'Programmerare'];
-  public originalsettings = ['Justering och inställning av skyddsanordningar och andra komponenter',
-  'Justering och inställning eller kontroll av maskinens funktionsparametrar (t.ex. hastighet, tryck, kraft, gränser för rörelser)',
-  'Fastspänning/fastsättning av arbetsstycket', 'Matning, påfyllning, laddning av råmaterial', 'Funktionsprovning, provkörning',
-  'Montering eller byte av verktyg, verktygsinställning', 'Kontroll av programmering', 'Kontroll av slutprodukten'];
-  public operation = ['4', ''];
-  public maintenance = ['5', ''];
-  public faultfinding = ['6', ''];
-  public scrapping = ['7', ''];
+  public originalsettings = ['Elinstallatör', 'Mekaniker', 'Programmerare'];
+  // tslint:disable-next-line: max-line-length
+  public operation = ['Operatör', 'Trainee', 'Praktikant', 'Lärling', 'Operatör av intilliggande maskin', 'Administrativ personal', 'Besökare', 'Allmänheten', 'Barn'];
+  public maintenance = ['Underhållspersonal', 'Servicepersonal'];
+  public faultfinding = ['Tekniker', 'Servicepersonal', 'Mekaniker', 'Elektriker'];
+  public scrapping = ['Elinstallatör', 'Mekaniker', 'Truckförare', 'Lastkopplare'];
 
   constructor() { }
 
